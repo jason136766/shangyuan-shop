bec53bdd101533e46ec095e6f2558e4bd75fc2ba
Armin <armin@ArmindeMacBook-Pro-2.local>
2019-05-17 21:25:15 +0700

    1 ;

--- src/views/Mall.vue
+++ src/views/Mall.vue
@@ -59,6 +59,30 @@
 
         <!-- 广告 1 区域 -->
         <div class="pr-3 pl-3 mt-3"><img src="@/static/images/advert.png" alt="广告2" width="100%" height="auto"></div>
+        
+        <!-- 广告 2 区域 -->
+        <van-row class="pr-3 pl-3 mt-3" gutter="5">
+            <van-col span="9"><img src="@/static/images/advert-1.png" alt="广告" width="100%" height="auto"></van-col>
+            <van-col span="15">
+                <van-row span="24" class="mb-1"><img src="@/static/images/advert-2.png" alt="广告" width="100%" height="auto"></van-row>
+                <van-row gutter="5">
+                    <van-col span="12"><img src="@/static/images/advert-3.png" alt="广告" width="100%" height="auto"></van-col>
+                    <van-col span="12"><img src="@/static/images/advert-3.png" alt="广告" width="100%" height="auto"></van-col>
+                </van-row>
+            </van-col>
+        </van-row>
+
+        <!--商店分类 -->
+        <div class="bg-white ml-3 mr-3 mt-3 text-center pt-2 pl-2 pr-2 font-15 rounded-lg tab">
+            <van-tabs v-model="active" :line-width="lineWidth" color="#4DA9FF" title-active-color="#4DA9FF" :line-height=lineHeight title-inactive-color="#000" swipe-threshold=6>
+                <van-tab title="享美食">享美食</van-tab>
+                <van-tab title="惠生活">惠生活</van-tab>
+                <van-tab title="爱玩乐">爱玩乐</van-tab>
+                <van-tab title="住酒店">住酒店</van-tab>
+                <van-tab title="时尚购">时尚购</van-tab>
+            </van-tabs>
+        </div>
+
     </div>
 </template>
 
@@ -71,6 +95,9 @@
         },
         data() {
             return {
+                lineHeight: 2,
+                lineWidth: 20,
+                active: 0,
                 searchIcon: '\ue637 上海 \ue61b',
                 slides: [
                     require('@/static/images/banner-5.png'),
@@ -118,8 +145,10 @@
             }
         }
     }
-    .category{
 
+    .tab{
+        margin-bottom: 10rem;
     }
+
 
 </style>
