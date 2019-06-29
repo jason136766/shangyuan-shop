789987c5e759503656f8f934e42356048871bd69
Armin <971832767@qq.com>
2019-06-29 14:22:33 +0700

    add my page

--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import { Row, Col, Search, Swipe, SwipeItem, Lazyload, Tab, Tabs, Tabbar, TabbarItem, Collapse, CollapseItem, Rate, DropdownMenu, DropdownItem } from 'vant';
+import { Row, Col, Search, Swipe, SwipeItem, Lazyload, Tab, Tabs, Tabbar, TabbarItem, Collapse, CollapseItem, Rate, DropdownMenu, DropdownItem, Image } from 'vant';
 
 
-Vue.use(Row).use(Col).use(Search).use(Swipe).use(SwipeItem).use(Lazyload).use(Tab).use(Tabs).use(Tabbar).use(TabbarItem).use(Collapse).use(CollapseItem).use(Rate).use(DropdownMenu).use(DropdownItem);
+Vue.use(Row).use(Col).use(Search).use(Swipe).use(SwipeItem).use(Lazyload).use(Tab).use(Tabs).use(Tabbar).use(TabbarItem).use(Collapse).use(CollapseItem).use(Rate).use(DropdownMenu).use(DropdownItem).use(Image);
 
 Vue.config.productionTip = false
 
--- src/views/My.vue
+++ src/views/My.vue
@@ -32,6 +32,134 @@
         </header>
 
         <!-- 我的订单 -->
+        <div class="p-2 bg-white rounded-lg my-order ml-3 mr-3 mt-2">
+            <van-row class="mb-3 pl-1 pr-1">
+                <van-col span="24">
+                    <div class="float-left text-secondary"><i class="iconfont font-weight-bold theme-color font-18">&#xe653;</i> 我的订单</div>
+                    <div class="float-right font-11 pt-2 text-999">全部 <i class="iconfont font-10">&#xe628;</i></div>
+                </van-col>
+            </van-row>
+            <van-row type="flex" class="text-666 pb-1 text-center" gutter="15">
+                <van-col span="6">
+                    <i class="iconfont font-20">&#xe621;</i>
+                    <div>待付款</div>
+                </van-col>
+                <van-col span="6" >
+                    <i class="iconfont font-20">&#xe615;</i>
+                    <div>待发货</div>
+                </van-col>
+                <van-col span="6">
+                    <i class="iconfont font-22">&#xe640;</i>
+                    <div>待收货</div>
+                </van-col>
+                <van-col span="6">
+                    <i class="iconfont font-20">&#xe623;</i>
+                    <div>待评价</div>
+                </van-col>
+            </van-row>
+        </div>
+
+        <!-- 我的资产 -->
+        <div class="p-2 bg-white rounded-lg my-order ml-3 mr-3 mt-3 mb-3">
+            <van-row class="mb-3 pl-1 pr-1">
+                <van-col span="24" class="text-secondary">
+                    <i class="iconfont theme-color font-20">&#xe668;</i> 我的资产
+                </van-col>
+            </van-row>
+            <van-row type="flex" class="text-666 text-center" gutter="20">
+                <van-col span="6">
+                    <i class="iconfont font-20">&#xe608;</i>
+                    <div>我的钱包</div>
+                </van-col>
+                <van-col span="6">
+                    <i class="iconfont font-20">&#xe644;</i>
+                    <div>零钱余额</div>
+                </van-col>
+                <van-col span="6">
+                    <i class="iconfont font-22">&#xe602;</i>
+                    <div>红包卡券</div>
+                </van-col>
+            </van-row>
+        </div>
+
+        <!-- 广告1区域 -->
+        <img src="@/static/images/my-advert-1.png" alt="广告" width="100%" height="auto">
+
+        <!-- 必备工具 -->
+        <div class="font-13 bg-white mt-3 rounded-lg">
+            <van-row class="p-2">
+                <van-col span="24" class="pl-1 font-15 text-secondary">必备工具</van-col>
+            </van-row>
+            <van-row class="text-center text-black-50">
+                <van-col span="24">
+                    <van-col span="6" class="border-top border-right pt-2 pb-2">
+                        <i class="iconfont font-18">&#xe66a;</i>
+                        <div>收藏宝贝</div>
+                    </van-col>
+                    <van-col span="6" class="border-top border-right pt-2 pb-2">
+                        <i class="iconfont font-18">&#xe605;</i>
+                        <div>关注店铺</div>
+                    </van-col>
+                    <van-col span="6" class="border-top border-right pt-2 pb-2">
+                        <i class="iconfont font-18">&#xe60a;</i>
+                        <div>退款/售后</div>
+                    </van-col>
+                    <van-col span="6" class="border-top pt-2 pb-2">
+                        <i class="iconfont font-18">&#xe60c;</i>
+                        <div>足迹</div>
+                    </van-col>
+                </van-col>
+                <van-col span="24">
+                    <van-col span="6" class="border-top border-right pt-2 pb-2">
+                        <i class="iconfont font-18">&#xe66a;</i>
+                        <div>收藏宝贝</div>
+                    </van-col>
+                    <van-col span="6" class="border-top border-right pt-2 pb-2">
+                        <i class="iconfont font-18">&#xe605;</i>
+                        <div>关注店铺</div>
+                    </van-col>
+                    <van-col span="6" class="border-top border-right pt-2 pb-2">
+                        <i class="iconfont font-18">&#xe60a;</i>
+                        <div>退款/售后</div>
+                    </van-col>
+                    <van-col span="6" class="border-top pt-2 pb-2">
+                        <i class="iconfont font-18">&#xe60c;</i>
+                        <div>足迹</div>
+                    </van-col>
+                </van-col>
+            </van-row>
+        </div>
+
+        <!-- 为你推荐 -->
+        <div class="text-center text-muted font-13 mt-3 mb-2">
+            <div class="d-inline-flex align-items-center">
+                ------ 为你推荐 ------
+            </div>
+        </div>
+
+        <van-row class="pt-2 pb-2 mb-5 ml-2 mr-2 rounded-lg" gutter="10" type="flex">
+            <van-col span="12" class="mb-2 good ml-1">
+                <div class="bg-white rounded-lg">
+                    <img src="@/static/images/recommend-goods.png" alt="广告2" width="100%" height="auto">
+                    <div class="pl-2 pr-2 pb-1 mt-1 font-13">
+                        <div class="line-ellipsis">拉夏贝尔2017春装新款 复古拼接五分袖短款假两件小衫哈哈哈...</div>
+                        <div class="theme-color">￥699.00</div>
+                        <div class="font-10 theme-color">抵扣零钱：260币</div>
+                    </div>
+                </div>
+            </van-col>
+            <van-col span="12" class="mb-2 good mr-1">
+                <div class="bg-white rounded-lg">
+                    <img src="@/static/images/recommend-goods.png" alt="广告2" width="100%" height="auto">
+                    <div class="pl-2 pr-2 pb-1 mt-1 font-13">
+                        <div class="line-ellipsis">拉夏贝尔2017春装新款 复古拼接五分袖短款假两件小衫哈哈哈...</div>
+                        <div class="theme-color">￥699.00</div>
+                        <div class="font-10 theme-color">抵扣零钱：260币</div>
+                    </div>
+                </div>
+            </van-col>
+        </van-row>
+
     </div>
 </template>
 
@@ -42,6 +170,10 @@
 </script>
 
 <style scoped lang="scss">
+    .my {
+        min-height: 780px;
+    }
+
     .top {
         position: relative;
         z-index: 1;
@@ -78,6 +210,12 @@
         position: absolute;
         right: 0.7rem;
         top: 4.7rem
+    }
+
+    .my-order {
+        position: relative;
+        z-index: 2;
+        font-size: 13px;
     }
 
     @media (max-width: 360px) {
